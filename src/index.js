6c9c9663c8be687849f40bfef84cbbd8be563106
Trent Lennon <tlennon@ebsco.com>
2016-10-03 14:19:35 -0400

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+var ComicSeries = require('./ComicSeries.jsx');
+
+var COMICS = [
+  {comicName: "Amazing Spider-man (2014-2015)", inPrint: false},
+  {comicName: "Amazing Spider-man (2015-2016)", inPrint: true}
+  
+]
+
+var PullList = React.createClass({
+  getInitialState: function() {
+    return {
+      comics: COMICS
+    }
+  },
+
+  render: function() {
+    return (
+      <div id='app'>Hello World!
+      <ComicSeries comics={this.props.comics}/>
+      </div>
+      );
+  }
+});
 
 ReactDOM.render(
-  <App />,
+  <PullList comics={COMICS} />,
   document.getElementById('root')
 );
