86e624aaa271eb5d2dd5d11c061c16bd704ae8c5
Trent Lennon <tlennon@ebsco.com>
2016-10-05 11:41:40 -0400

    Moved the private and public keys out of the server.js. They are now stored in Environment Variables.

--- server.js
+++ server.js
@@ -1,26 +1,62 @@
 const express = require('express');
 const app = express();
-var fs = require('fs');
+var crypto = require('crypto');
+var request = require('request');
 
 var API_PORT = 3001;
-var comicJSONLocation = "./data/comic.json";
 
-var COMICS = [
-  {"comicName": "Amazing Spider-man 2014", "inPrint": false}
-];
+var PRIVATE_KEY = process.env.MARVEL_PRIVATE_KEY;
+var PUBLIC_KEY  = process.env.MARVEL_PUBLIC_KEY;
 
+var limit = 100;
+var ts = 1234512;
+
+var hashBuilder = `${ts}${PRIVATE_KEY}${PUBLIC_KEY}`;
+var hashValue = crypto.createHash('md5').update(hashBuilder).digest("hex");
+
+var queryString = `http://gateway.marvel.com:80/v1/public/series?contains=comic&seriesType=ongoing&limit=${limit}&apikey=${PUBLIC_KEY}&ts=${ts}&hash=${hashValue}`;
+//https://imagecomics.com/comics/series/P675
 app.set('port', (API_PORT || 3001));
 
+var lastTimeRequestWasMade;
+var savedData;
+
 app.get('/api/comics', (req, res) => {
-    fs.readFile(comicJSONLocation, function(err,data) {
-        if(err) throw err
+  var rightNowDate = getCurrentDateFormatted();
+  
+  if( lastTimeRequestWasMade === null || rightNowDate !== lastTimeRequestWasMade) {
+    lastTimeRequestWasMade = rightNowDate
+    
+    console.log(`Requesting data from Marvel.com on ${rightNowDate}`);
+    //TODO: Loop the request until we get all the results, adding all of them into the JSON array.
+    request.get({
+      url: queryString,
+      json: true
+    }, (err, resp, data) => {
+      console.log("Storing data for subsequent retrieval.");
 
-        res.json(JSON.parse(data));
+      savedData = data;
+      res.send(savedData);
+      return;
     });
-
+  } 
+  else {
+    res.send(savedData);
     return;
+  }
 });
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  console.log(`Private key: ${PRIVATE_KEY}`);
+  console.log(`Public key: ${PUBLIC_KEY}`);
+  
 });
+
+function getCurrentDateFormatted() {
+  var date = new Date();
+  var year = date.getFullYear();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  return `${month}/${day}/${year}`;
+}
